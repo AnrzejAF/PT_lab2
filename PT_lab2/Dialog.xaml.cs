db5cd7cf7f01051c8aca02447b06d31e8a511880
Anrzej <andrzejfijalo@gmail.com>
2024-04-01 19:48:50 +0200

    add dialog window add file viewer

---
+++ PT_lab2/Dialog.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PT_lab2
+{
+    /// <summary>
+    /// Interaction logic for CreateDialog.xaml
+    /// </summary>
+    public partial class Dialog : Window
+    {
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        //public string CreatedName { get; private set; }
+        //public FileAttributes CreatedAttributes
+        //{
+        //    get
+        //    {
+        //        FileAttributes attributes = default;
+        //        if (ReadOnlyCheckBox.IsChecked == true) attributes |= FileAttributes.ReadOnly;
+        //        if (HiddenCheckBox.IsChecked == true) attributes |= FileAttributes.Hidden;
+        //        if (SystemCheckBox.IsChecked == true) attributes |= FileAttributes.System;
+        //        return attributes;
+        //    }
+        //}
+        //public bool IsFile => FileRadioButton.IsChecked == true;
+
+        //public Dialog()
+        //{
+        //    InitializeComponent();
+        //}
+
+        //private void OkButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    this.CreatedName = NameTextBox.Text;
+        //    this.DialogResult = true;
+        //}
+
+        //private void CancelButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    this.DialogResult = false;
+        //}
+    }
+
+
+}
--- PT_lab2/MainWindow.xaml.cs
+++ PT_lab2/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Forms;
+using System.Collections.ObjectModel;
+using IOPath = System.IO.Path;
 
 namespace PT_lab2
 {
@@ -24,61 +26,164 @@ namespace PT_lab2
         public MainWindow()
         {
             InitializeComponent();
+            makeTreeView(@"D:\temp");
+
         }
 
         private void Exit_Click(object sender, RoutedEventArgs e)
         {
-
+            this.Close();
         }
+
         // create open browse path dialog and add them to TreeViewItem
         private void Open_Click(object sender, RoutedEventArgs e)
         {
-            // if reinitiaze the dialog clear the treeView
-            treeView.Items.Clear();
             System.Windows.Forms.FolderBrowserDialog folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
             folderBrowserDialog.ShowDialog();
             string path = folderBrowserDialog.SelectedPath;
-            // foreach loop to add all directories and files to the treeView, but only show name of the file or directory
-            foreach (string dir in System.IO.Directory.GetDirectories(path))
+            
+            makeTreeView(path);
+        }
+
+        private void FoldersTreeView_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            TreeViewItem item = (TreeViewItem)sender;
+            item.ContextMenu = new ContextMenu();
+            MenuItem createMenuItem = new MenuItem();
+            createMenuItem.Header = "Create";
+            createMenuItem.Click += CreateMenuItem_Click;
+            MenuItem deleteMenuItem = new MenuItem();
+            deleteMenuItem.Header = "Delete";
+            deleteMenuItem.Click += DeleteMenuItem_Click;
+            item.ContextMenu.Items.Add(createMenuItem);
+            item.ContextMenu.Items.Add(deleteMenuItem);
+        }
+
+        private void FileTreeView_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            TreeViewItem item = (TreeViewItem)sender;
+            item.ContextMenu = new ContextMenu();
+            MenuItem createMenuItem = new MenuItem();
+            createMenuItem.Header = "Create";
+            createMenuItem.Click += CreateMenuItem_Click;
+            MenuItem deleteMenuItem = new MenuItem();
+            deleteMenuItem.Header = "Delete";
+            deleteMenuItem.Click += DeleteMenuItem_Click;
+            MenuItem openMenuItem = new MenuItem();
+            openMenuItem.Header = "Open";
+            openMenuItem.Click += OpenFile_Click;
+            item.ContextMenu.Items.Add(openMenuItem);
+            item.ContextMenu.Items.Add(createMenuItem);
+            item.ContextMenu.Items.Add(deleteMenuItem);
+        }
+
+        private void CreateMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            // delete selected item from treeView and from the file system
+            if (FoldersTreeView.SelectedItem is TreeViewItem selectedItem && selectedItem.Tag is string path)
             {
-                TreeViewItem item = new TreeViewItem();
-                item.Header = System.IO.Path.GetFileName(dir);
-                treeView.Items.Add(item);
-                foreach (string file in System.IO.Directory.GetFiles(dir))
+                if (System.IO.Directory.Exists(path))
                 {
-                    TreeViewItem fileItem = new TreeViewItem();
-                    fileItem.Header = System.IO.Path.GetFileName(file);
-                    item.Items.Add(fileItem);
+                    System.IO.Directory.Delete(path, true);
                 }
+                else if (System.IO.File.Exists(path))
+                {
+                    System.IO.File.Delete(path);
+                }
+                selectedItem.Items.Clear();
+                FoldersTreeView.Items.Remove(selectedItem);
             }
         }
 
-        // add contexr menu to the treeView to read the file content and show it in the textBox
-        private void treeView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
         {
-            TreeViewItem item = (TreeViewItem)sender;
-            System.Windows.Controls.ContextMenu contextMenu = new System.Windows.Controls.ContextMenu();
-            System.Windows.Controls.MenuItem menuItem = new System.Windows.Controls.MenuItem();
-            menuItem.Header = "Read";
-            menuItem.Click += new RoutedEventHandler(Read_Click);
-            contextMenu.Items.Add(menuItem);
-            item.ContextMenu = contextMenu;
-        }
-
-        private void Read_Click(object sender, RoutedEventArgs e)
-        {
-            TreeViewItem item = (TreeViewItem)sender;
-            string path = item.Header.ToString();
-            //textBox.Text = System.IO.File.ReadAllText(path)
-        }
-
-        private void Add_Click(object sender, RoutedEventArgs e)
-        {
+            try
+            {
+                if (FoldersTreeView.SelectedItem is TreeViewItem selectedItem && selectedItem.Tag is string filePath)
+                {
+                    using (var textReader = System.IO.File.OpenText(filePath))
+                    {
+                        string text = textReader.ReadToEnd();
+                        FileContent.Text = text;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show(ex.Message);
+            }
 
         }
 
-        private void Delete_Click(object sender, RoutedEventArgs e)
+        private void makeTreeView(string path) // TODO
         {
+            FoldersTreeView.Items.Clear();
+
+            if (System.IO.Directory.Exists(path))
+            {
+                string[] directories = System.IO.Directory.GetDirectories(path);
+                string[] files = System.IO.Directory.GetFiles(path);
+
+                foreach (string directory in directories)
+                {
+                    TreeViewItem item = new TreeViewItem();
+                    item.Header = IOPath.GetFileName(directory);
+                    item.Tag = directory;
+                    item.MouseRightButtonDown += FoldersTreeView_MouseRightButtonDown;
+                    FoldersTreeView.Items.Add(item);
+                    // add all files in the directory
+                    string[] subFiles = System.IO.Directory.GetFiles(directory);
+                    foreach (string subFile in subFiles)
+                    {
+                        TreeViewItem subItem = new TreeViewItem();
+                        subItem.Header = IOPath.GetFileName(subFile);
+                        subItem.Tag = subFile;
+                        subItem.MouseRightButtonDown += FileTreeView_MouseRightButtonDown;
+                        item.Items.Add(subItem);
+                    }
+                    // add also subdirectories and files
+                    string[] subDirectories = System.IO.Directory.GetDirectories(directory);
+                    foreach (string subDirectory in subDirectories)
+                    {
+                        TreeViewItem subItem = new TreeViewItem();
+                        subItem.Header = IOPath.GetFileName(subDirectory);
+                        subItem.Tag = subDirectory;
+                        subItem.MouseRightButtonDown += FoldersTreeView_MouseRightButtonDown;
+                        item.Items.Add(subItem);
+                        // add all files in the subdirectory
+                        string[] subSubFiles = System.IO.Directory.GetFiles(subDirectory);
+                        foreach (string subFile in subSubFiles)
+                        {
+                            TreeViewItem subSubItem = new TreeViewItem();
+                            subSubItem.Header = IOPath.GetFileName(subFile);
+                            subSubItem.Tag = subFile;
+                            subSubItem.MouseRightButtonDown += FileTreeView_MouseRightButtonDown;
+                            subItem.Items.Add(subSubItem);
+                        }
+                    }
+                }
+            }   
+        }
+
+        private void addTreeViewItem(TreeViewItem item, string path)
+        {
+            item.Header = IOPath.GetFileName(path);
+            item.Tag = path;
+            item.MouseRightButtonDown += FoldersTreeView_MouseRightButtonDown;
+            FoldersTreeView.Items.Add(item);
+        }
+        // refresh the treeView automatically when the selected item is changed
+        private void FoldersTreeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            if (FoldersTreeView.SelectedItem is TreeViewItem selectedItem && selectedItem.Tag is string path)
+            {
+                makeTreeView(path);
+            }
 
         }
     }
