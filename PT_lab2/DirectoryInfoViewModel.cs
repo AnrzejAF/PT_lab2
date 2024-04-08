cdd01653c8adb711f0f55b5e7b496ef83061e393
Anrzej <andrzejfijalo@gmail.com>
2024-04-08 22:16:56 +0200

    add class for second lab

---
+++ PT_lab2/DirectoryInfoViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PT_lab2
+{
+    public class DirectoryInfoViewModel : FileSystemInfoViewModel
+    {
+        public Exception? Exception { get; private set; }
+
+        public ObservableCollection<FileSystemInfoViewModel> Items { get; private set; }
+            = new ObservableCollection<FileSystemInfoViewModel>();
+
+
+        public bool Open(string path)
+        {
+            bool result = false;
+            try
+            {
+                foreach (var dirName in Directory.GetDirectories(path))
+                {
+                    var dirInfo = new DirectoryInfo(dirName);
+                    DirectoryInfoViewModel itemViewModel = new DirectoryInfoViewModel();
+                    itemViewModel.Model = dirInfo;
+                    Items.Add(itemViewModel);
+                }
+                foreach (var fileName in Directory.GetFiles(path))
+                {
+                    var fileInfo = new FileInfo(fileName);
+                    FileInfoViewModel itemViewModel = new FileInfoViewModel();
+                    itemViewModel.Model = fileInfo;
+                    Items.Add(itemViewModel);
+                }
+                result = true;
+            }
+            catch (Exception ex)
+            {
+                Exception = ex;
+            }
+            return result;
+        }
+
+    }
+
+}
---
+++ PT_lab2/FileSystemInfoViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PT_lab2
+{
+    public class FileSystemInfoViewModel : ViewModelBase
+    {
+        public DateTime LastWriteTime
+        {
+            get { return _lastWriteTime; }
+            set
+            {
+                if (_lastWriteTime != value)
+                {
+                    _lastWriteTime = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        private DateTime _lastWriteTime;
+
+        public DateTime LastAccessTime
+        {
+            get { return _lastAccessTime; }
+            set
+            {
+                if (_lastAccessTime != value)
+                {
+                    _lastAccessTime = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        private DateTime _lastAccessTime;
+
+        public FileSystemInfo Model
+        {
+            get { return _fileSystemInfo; }
+            set
+            {
+                if (_fileSystemInfo != value)
+                {
+                    _fileSystemInfo = value;
+                    this.LastWriteTime = value.LastWriteTime;
+                    this.LastAccessTime = value.LastAccessTime;
+
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        private FileSystemInfo ?_fileSystemInfo;
+    }
+}
